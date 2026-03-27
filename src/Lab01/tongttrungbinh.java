100cb4f7dea9da485e123b1c383c2fa5a03ab8c9
DO TRI AN <dotrian10@gmail.com>
2026-03-28 01:05:51 +0700

    Complete assingment 6.5

---
+++ src/Lab01/tongttrungbinh.java
@@ -0,0 +1,27 @@
+package Lab01;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class tongttrungbinh {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            System.out.print("arr[" + i + "] = ");
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int x : arr) {
+            sum += x;
+        }
+
+        double avg = (double) sum / n;
+        Arrays.sort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        System.out.println("Sum = " + sum);
+        System.out.println("Average = " + avg);
+        sc.close();
+    }
+}
