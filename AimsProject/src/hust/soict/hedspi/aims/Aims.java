ef62da183ee87399fc98147436e1b6456b5db355
DO TRI AN <dotrian10@gmail.com>
2026-04-22 18:10:50 +0700

    Re-á»organize Project

---
+++ AimsProject/src/hust/soict/hedspi/aims/Aims.java
@@ -0,0 +1,19 @@
+package hust.soict.hedspi.aims;
+
+import hust.soict.hedspi.aims.cart.Cart;
+import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+
+public class Aims {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc(
+                "The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc(
+                "Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.displayCart();
+    }
+}
